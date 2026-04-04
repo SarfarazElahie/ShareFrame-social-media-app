f3a9e1e3bcf64834c067356b7fd7b6aff41d02e3
Sarfaraz Elahie <sarfarazelahie62@gmail.com>
2026-04-04 23:06:33 +0530

    use try catch in post.controller.js

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -2,7 +2,8 @@ const postModel = require("../models/post.model.js")
 const uploadFile = require("../services/storage.service.js")
 
 const createPost = async(req, res)=>{
-    const result = await uploadFile(req.file.buffer);
+    try {
+        const result = await uploadFile(req.file.buffer);
     
         const post = await postModel.create({
             image: result.url,
@@ -13,15 +14,30 @@ const createPost = async(req, res)=>{
             message: "Post created succesfully",
             post
         })
+    } catch (error) {
+         return res.status(500).json({
+            message: "Error creating post",
+            error: error.message
+        });
+    }
+    
 }
 
 const getPosts = async(req, res)=>{
-     const posts = await postModel.find()
+    try {
+        const posts = await postModel.find()
 
-    return res.status(200).json({
-        message: "Posts fetched succesfully",
-        posts
-    })
+        return res.status(200).json({
+            message: "Posts fetched succesfully",
+            posts
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: "Error fetching posts",
+            error: error.message
+        });
+    }
+    
 }
 
 module.exports = {createPost, getPosts};
