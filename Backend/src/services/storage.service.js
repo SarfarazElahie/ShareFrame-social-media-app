8f524b8017c5dda17685416053d8f1e493115216
Sarfaraz Elahie <sarfarazelahie62@gmail.com>
2026-04-04 23:24:34 +0530

    use try catch in storage.service.js file and add public key and url end point

--- Backend/src/services/storage.service.js
+++ Backend/src/services/storage.service.js
@@ -1,16 +1,21 @@
 const ImageKit = require("@imagekit/nodejs");
 
 const imageKit = new ImageKit({
-    privateKey : process.env.IMAGEKIT_PRIVATE_KEY
+    privateKey : process.env.IMAGEKIT_PRIVATE_KEY,
+    publicKey: process.env.IMAGEKIT_PUBLIC_KEY,
+    urlEndpoint: process.env.IMAGEKIT_URL_ENDPOINT
 });
 
 async function uploadFile(buffer, name= "upload.jpg") {
-    const result = await imageKit.files.upload({
+    try {
+        const result = await imageKit.files.upload({
         file: buffer.toString("base64"),
         fileName : name
     })
-
     return result;
+    } catch (error) {
+        console.error("ImageKit Upload Error:", error.message)
+    }
 }
 
 module.exports = uploadFile
