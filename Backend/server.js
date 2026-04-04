80fc5c808b711463c2550e954129b7cc62bb7b5c
Sarfaraz Elahie <sarfarazelahie62@gmail.com>
2026-04-05 00:01:38 +0530

    make server port dynamic acc to .env file

--- Backend/server.js
+++ Backend/server.js
@@ -4,7 +4,8 @@ const connectDB = require("./src/db/db.js")
 
 connectDB();
 
+const PORT = process.env.PORT || 3000;
 
 app.listen(3000, ()=>{
-    console.log("Server is running.....")
+    console.log(`Server is running on port ${PORT}.....`)
 })
